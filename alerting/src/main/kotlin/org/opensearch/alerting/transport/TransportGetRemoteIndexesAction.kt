d884eed7734a8c16a0b070cec5dc3b890efff7f1
AWSHurneyt <hurneyt@amazon.com>
2024-02-02 13:32:13 -0800

    Adjusted thread stashing.
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/transport/TransportGetRemoteIndexesAction.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/transport/TransportGetRemoteIndexesAction.kt
@@ -8,6 +8,8 @@ package org.opensearch.alerting.transport
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
 import org.apache.logging.log4j.LogManager
 import org.opensearch.action.ActionListener
 import org.opensearch.action.admin.cluster.health.ClusterHealthRequest
@@ -82,83 +84,86 @@ class TransportGetRemoteIndexesAction @Inject constructor(
 
         client.threadPool().threadContext.stashContext().use {
             scope.launch {
-                it.restore()
-                val clusterIndexesList = mutableListOf<ClusterIndexes>()
+                val singleThreadContext = newSingleThreadContext("${GetRemoteIndexesAction.NAME}_thread")
+                withContext(singleThreadContext) {
+                    it.restore()
+                    val clusterIndexesList = mutableListOf<ClusterIndexes>()
 
-                var resolveIndexResponse: ResolveIndexAction.Response? = null
-                try {
-                    resolveIndexResponse = getRemoteClusters(CrossClusterMonitorUtils.parseIndexesForRemoteSearch(request.indexes, clusterService))
-                    log.info("hurneyt TransportGetRemoteIndexesAction::resolveIndexResponse = {}", resolveIndexResponse.convertToMap())
-                } catch (e: Exception) {
-                    log.error("Failed to retrieve indexes for request $request", e)
-                    actionListener.onFailure(AlertingException.wrap(e))
-                }
-
-                val resolvedIndexes: MutableList<String> = mutableListOf()
-                if (resolveIndexResponse != null) {
-                    resolveIndexResponse.indices.forEach { resolvedIndexes.add(it.name) }
-                    resolveIndexResponse.aliases.forEach { resolvedIndexes.add(it.name) }
-                }
-                log.info("hurneyt TransportGetRemoteIndexesAction::resolvedIndexes = {}", resolvedIndexes)
-
-                val clusterIndexesMap = CrossClusterMonitorUtils.separateClusterIndexes(resolvedIndexes, clusterService)
-                log.info("hurneyt TransportGetRemoteIndexesAction::clusterIndexesMap = {}", clusterIndexesMap)
-
-                clusterIndexesMap.forEach { (clusterName, indexes) ->
-                    log.info("hurneyt TransportGetRemoteIndexesAction::clusterIndexesMap clusterName = {}", clusterName)
-                    log.info("hurneyt TransportGetRemoteIndexesAction::clusterIndexesMap indexes = {}", indexes)
-                    val targetClient = CrossClusterMonitorUtils.getClientForCluster(clusterName, client, clusterService)
-
-                    val startTime = Instant.now()
-                    var clusterHealthResponse: ClusterHealthResponse? = null
+                    var resolveIndexResponse: ResolveIndexAction.Response? = null
                     try {
-                        clusterHealthResponse = getHealthStatuses(targetClient, indexes)
+                        resolveIndexResponse = getRemoteClusters(CrossClusterMonitorUtils.parseIndexesForRemoteSearch(request.indexes, clusterService))
+                        log.info("hurneyt TransportGetRemoteIndexesAction::resolveIndexResponse = {}", resolveIndexResponse.convertToMap())
                     } catch (e: Exception) {
-                        log.error("Failed to retrieve health statuses for request $request", e)
+                        log.error("Failed to retrieve indexes for request $request", e)
                         actionListener.onFailure(AlertingException.wrap(e))
                     }
-                    val endTime = Instant.now()
 
-                    log.info("hurneyt TransportGetRemoteIndexesAction::clusterHealthResponse keys = {}", clusterHealthResponse?.indices?.keys)
+                    val resolvedIndexes: MutableList<String> = mutableListOf()
+                    if (resolveIndexResponse != null) {
+                        resolveIndexResponse.indices.forEach { resolvedIndexes.add(it.name) }
+                        resolveIndexResponse.aliases.forEach { resolvedIndexes.add(it.name) }
+                    }
+                    log.info("hurneyt TransportGetRemoteIndexesAction::resolvedIndexes = {}", resolvedIndexes)
 
-                    val latency = Duration.between(startTime, endTime).toMillis()
+                    val clusterIndexesMap = CrossClusterMonitorUtils.separateClusterIndexes(resolvedIndexes, clusterService)
+                    log.info("hurneyt TransportGetRemoteIndexesAction::clusterIndexesMap = {}", clusterIndexesMap)
 
-                    var mappingsResponse: GetMappingsResponse? = null
-                    if (request.includeMappings) {
+                    clusterIndexesMap.forEach { (clusterName, indexes) ->
+                        log.info("hurneyt TransportGetRemoteIndexesAction::clusterIndexesMap clusterName = {}", clusterName)
+                        log.info("hurneyt TransportGetRemoteIndexesAction::clusterIndexesMap indexes = {}", indexes)
+                        val targetClient = CrossClusterMonitorUtils.getClientForCluster(clusterName, client, clusterService)
+
+                        val startTime = Instant.now()
+                        var clusterHealthResponse: ClusterHealthResponse? = null
                         try {
-                            mappingsResponse = getIndexMappings(targetClient, indexes)
+                            clusterHealthResponse = getHealthStatuses(targetClient, indexes)
                         } catch (e: Exception) {
-                            log.error("Failed to retrieve mappings for request $request", e)
+                            log.error("Failed to retrieve health statuses for request $request", e)
                             actionListener.onFailure(AlertingException.wrap(e))
                         }
-                    }
+                        val endTime = Instant.now()
 
-                    val clusterIndexList = mutableListOf<ClusterIndex>()
-                    if (clusterHealthResponse != null) {
-                        indexes.forEach {
-                            clusterIndexList.add(
-                                ClusterIndex(
-                                    indexName = it,
-                                    indexHealth = clusterHealthResponse.indices[it]!!.status,
-                                    mappings = mappingsResponse?.mappings?.get(it)
-                                )
-                            )
+                        log.info("hurneyt TransportGetRemoteIndexesAction::clusterHealthResponse keys = {}", clusterHealthResponse?.indices?.keys)
+
+                        val latency = Duration.between(startTime, endTime).toMillis()
+
+                        var mappingsResponse: GetMappingsResponse? = null
+                        if (request.includeMappings) {
+                            try {
+                                mappingsResponse = getIndexMappings(targetClient, indexes)
+                            } catch (e: Exception) {
+                                log.error("Failed to retrieve mappings for request $request", e)
+                                actionListener.onFailure(AlertingException.wrap(e))
+                            }
                         }
-                    }
 
-                    clusterIndexesList.add(
-                        ClusterIndexes(
-                            clusterName = clusterName,
-                            clusterHealth = clusterHealthResponse!!.status,
-                            hubCluster = clusterName == clusterService.clusterName.value(),
-                            indexes = clusterIndexList,
-                            latency = latency
+                        val clusterIndexList = mutableListOf<ClusterIndex>()
+                        if (clusterHealthResponse != null) {
+                            indexes.forEach {
+                                clusterIndexList.add(
+                                    ClusterIndex(
+                                        indexName = it,
+                                        indexHealth = clusterHealthResponse.indices[it]!!.status,
+                                        mappings = mappingsResponse?.mappings?.get(it)
+                                    )
+                                )
+                            }
+                        }
+
+                        clusterIndexesList.add(
+                            ClusterIndexes(
+                                clusterName = clusterName,
+                                clusterHealth = clusterHealthResponse!!.status,
+                                hubCluster = clusterName == clusterService.clusterName.value(),
+                                indexes = clusterIndexList,
+                                latency = latency
+                            )
                         )
-                    )
+                    }
+                    log.info("hurneyt TransportGetRemoteIndexesAction::clusterIndexesList = {}", clusterIndexesList)
+                    log.info("hurneyt TransportGetRemoteIndexesAction END")
+                    actionListener.onResponse(GetRemoteIndexesResponse(clusterIndexes = clusterIndexesList))
                 }
-                log.info("hurneyt TransportGetRemoteIndexesAction::clusterIndexesList = {}", clusterIndexesList)
-                log.info("hurneyt TransportGetRemoteIndexesAction END")
-                actionListener.onResponse(GetRemoteIndexesResponse(clusterIndexes = clusterIndexesList))
             }
         }
     }
@@ -179,11 +184,9 @@ class TransportGetRemoteIndexesAction @Inject constructor(
             .indices(*parsedIndexesNames.toTypedArray())
             .indicesOptions(IndicesOptions.lenientExpandHidden())
 
-        return client.suspendUntil { targetClient.admin().cluster().health(clusterHealthRequest, it) }
-        // TODO hurneyt delete
-//        return targetClient.suspendUntil {
-//            admin().cluster().health(clusterHealthRequest, it)
-//        }
+        return targetClient.suspendUntil {
+            admin().cluster().health(clusterHealthRequest, it)
+        }
     }
 
     private suspend fun getIndexMappings(targetClient: Client, parsedIndexNames: List<String>): GetMappingsResponse {
